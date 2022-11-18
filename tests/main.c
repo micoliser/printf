5d98d841426554d1af28c7b8bd894a6cde0cea63
micoliser <micoliser@gmail.com>
2022-11-18 09:22:47 -0800

    create test folder

---
+++ tests/main.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+int main(void)
+{
+	int count;
+
+	count = _printf(NULL);
+	putchar('\n');
+	printf("%d\n", count);
+
+	count = _printf("%c cats", 'c');
+	putchar('\n');
+	printf("%d\n", count);
+
+	count = _printf("%s hahahsa", "Emiline");
+	putchar('\n');
+	printf("%d\n", count);
+
+	count = _printf("%% hey %s", NULL);
+	putchar('\n');
+	printf("%d\n", count);
+
+	count = _printf("%d hey %s", 5045, "Berny");
+	putchar('\n');
+	printf("%d\n", count);
+
+	count = _printf("%i", 5);
+	putchar('\n');
+	printf("%d\n", count);
+	
+	count = _printf("%d", -77383);
+	putchar('\n');
+	printf("%d\n", count);
+
+	count = _printf("%i", -2334);
+	putchar('\n');
+	printf("%d\n", count);
+
+	count = _printf("%i", 0);
+	putchar('\n');
+	printf("%d\n", count);
+
+	return (0);
+}
