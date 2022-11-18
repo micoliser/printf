0b3864a925c94e39a81160f1d65e9782b4313ae6
micoliser <micoliser@gmail.com>
2022-11-18 06:49:46 -0800

    make function work for i and d conversion specifiers

--- _printf.c
+++ _printf.c
@@ -1,6 +1,8 @@
 #include "main.h"
 
 int print_str(char *);
+void print_number(int n);
+int count_numbers(int n);
 
 /**
  * _printf - a function that produces output according to a format
@@ -11,7 +13,7 @@ int print_str(char *);
 int _printf(const char *format, ...)
 {
 	va_list list;
-	int i, count = 0;
+	int i, count = 0, num;
 	char *str;
 
 	if (!format)
@@ -24,19 +26,24 @@ int _printf(const char *format, ...)
 		{
 			switch (format[i + 1])
 			{
-			case 'c':
-				_putchar(va_arg(list, int)), i++, count++;
-				break;
-			case 's':
-				str = va_arg(list, char *);
-				count += print_str(str), i++;
-				break;
-			case '%':
-				_putchar('%'), i++, count++;
-				break;
-			default:
-				_putchar('%'), count++;
-				break;
+				case 'c':
+					_putchar(va_arg(list, int)), i++, count++;
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					count += print_str(str), i++;
+					break;
+				case '%':
+					_putchar('%'), i++, count++;
+					break;
+				case 'd': case 'i':
+					num = va_arg(list, int);
+					print_number(num);
+					count += count_numbers(num), i++;
+					break;
+				default:
+					_putchar('%'), count++;
+					break;
 			}
 			continue;
 		}
@@ -57,11 +64,62 @@ int print_str(char *s)
 	int i;
 
 	if (!s)
-		return(0);
+		return (0);
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		_putchar(s[i]);
 	}
+	return (i);
+}
+
+/**
+ * print_number - prints an integer
+ * @n: the integer to print
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+	{
+		print_number(num / 10);
+	}
+
+	_putchar((num % 10) + '0');
+}
+
+/**
+ * count_numbers - counts numbers
+ * @n: the number
+ *
+ * Return: count
+ */
+int count_numbers(int n)
+{
+	int i = 0, num = n;
+
+	if (n == 0)
+		return (1);
+
+	if (n < 0)
+	{
+		i++;
+		num *= -1;
+	}
+
+	while (num != 0)
+	{
+		i++;
+		num /= 10;
+	}
+
 	return (i);
 }
