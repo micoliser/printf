8aa2d77aff28d351d0eea4e5c40c0128546a7568
micoliser <micoliser@gmail.com>
2022-11-21 04:21:37 -0800

    changed number function

--- print_number.c
+++ print_number.c
@@ -8,36 +8,33 @@
  */
 int print_number(va_list args)
 {
-	int arr[100], i = 0, j, r, count = 0;
 	int num;
+	unsigned int num_t, temp, i, div = 1, count = 0;
 
 	num = va_arg(args, int);
-
-	if (num == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-
 	if (num < 0)
 	{
+		num_t = num * -1;
 		_putchar('-');
-		num = -num;
 		count++;
 	}
-
-	while (num != 0)
+	else
 	{
-		r = num % 10;
-		arr[i] = r;
-		i++;
-
-		num /= 10;
+		num_t = num;
 	}
 
-	for (j = i - 1; j > -1; j--, count++)
-		_putchar(arr[j] + '0');
+	temp = num_t;
 
+	while (temp > 9)
+	{
+		div *= 10;
+		temp /= 10;
+	}
+
+	for (i = 0; div > 0; div /= 10, i++, count++)
+	{
+		_putchar(((num_t / div) % 10) + '0');
+	}
 	return (count);
 }
 
