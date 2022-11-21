134b42d2aaebf3e4958b36f0a522e68428f9f90f
Jesulayomy <jesulayomi081@gmail.com>
2022-11-21 16:33:21 +0000

    Helo

--- tests/main.c
+++ tests/main.c
@@ -2,7 +2,21 @@
 
 int main(void)
 {
-	_printf("Hello\n");
+_printf("Octal: %o\n#Octal: %#o\nLOctal: %lo\nHOctal: %ho\n", 120, 120, 75, 89);
+printf("Octal: %o\n#Octal: %#o\nLOctal: %lo\nHOctal: %ho\n", 120, 120, 75, 89);
+_printf("______________________________________________________\n");
+_printf("hex: %x\n#hex: %#x\nLhex: %lx\nHhex: %hx\n", 120, 120, 120, 120);
+printf("hex: %x\n#hex: %#x\nLhex: %lx\nHhex: %hx\n", 120, 120, 120, 120);
+_printf("______________________________________________________\n");
+_printf("HEX: %X\n#HEX: %#X\nLHEX: %lX\nHHEX: %hX\n", 120, 120, 120, 120);
+printf("HEX: %X\n#HEX: %#X\nLHEX: %lX\nHHEX: %hX\n", 120, 120, 120, 120);
+_printf("______________________________________________________\n");
+_printf("Undigned-dec: %u\nLUndigned-dec: %lu\nHUndigned-dec: %hu\n", 120, 120, 120);
+printf("Undigned-dec: %u\nLUndigned-dec: %lu\nHUndigned-dec: %hu\n", 120, 120, 120);
+_printf("______________________________________________________\n");
+_printf("dec: %d\nLhex: %ld\nHhex: %hd\n", 120, 120, 120);
+printf("dec: %d\nLhex: %ld\nHhex: %hd\n", 120, 120, 120);
+_printf("______________________________________________________\n");
 
 	return (0);
 }
